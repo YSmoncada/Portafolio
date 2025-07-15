96de43e8dd1084824992afd39b46edabf07ad0fa
YSmoncada <yoinermoncadallanos@gmail.com>
2025-07-15 14:21:07 -0500

    padre dios santo

--- myapp/myapp/wsgi.py
+++ myapp/myapp/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.myapp.settings')
 
 application = get_wsgi_application()
 
